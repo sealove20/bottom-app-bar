117db4869312caa583bc67fced1b029e1d9e29a0
Matheus Henrique <matheus.181137@gmail.com>
2020-01-07 10:56:06 -0400

    Update ExampleOne.js

--- examples/ExampleOne.js
+++ examples/ExampleOne.js
@@ -1,35 +1,40 @@
 import React from "react";
 import { StyleSheet, Text, View, Alert } from "react-native";
+
 import { BottomAppContainer, BottomAppButton } from "bottom-app-bar";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>I'm an App!</Text>
-      <BottomAppContainer barColor={"#48c242"} buttonColor={"#ed952f"}>
+      <BottomAppContainer>
         <BottomAppButton
-          label={"Pokemons"}
+          label={"Cars"}
           buttonColor={"#fff"}
-          iconName={"pokeball"}
+          iconName={"car"}
+          iconColor={"#46f098"}
+          click={() => Alert.alert("Cars click")}
+        />
+        <BottomAppButton
+          label={"Buses"}
+          buttonColor={"#fff"}
+          iconName={"bus"}
           iconColor={"#bf440f"}
-          labelColor={"#f00"}
-          click={() => Alert.alert("Pokemons")}
+          click={() => Alert.alert("Buses click")}
         />
         <BottomAppButton
-          label={"Near by"}
-          buttonColor={"#a6e647"}
-          iconName={"pokemon-go"}
-          iconColor={"#f065a6"}
-          labelColor={"#cdf8fa"}
-          click={() => Alert.alert("Near by Pokemons")}
+          label={"Trains"}
+          buttonColor={"#fff"}
+          iconName={"train"}
+          iconColor={"#ded643"}
+          click={() => Alert.alert("Trains click")}
         />
         <BottomAppButton
-          label={"Duel"}
-          buttonColor={"#559be6"}
-          iconName={"sword-cross"}
-          iconColor={"#ebd34d"}
-          labelColor={"#b75ef2"}
-          click={() => Alert.alert("Duel Started")}
+          label={"Ships"}
+          buttonColor={"#fff"}
+          iconName={"ferry"}
+          iconColor={"#41b5d9"}
+          click={() => Alert.alert("Ships click")}
         />
       </BottomAppContainer>
     </View>
@@ -44,3 +49,4 @@ const styles = StyleSheet.create({
     justifyContent: "center"
   }
 });
+
